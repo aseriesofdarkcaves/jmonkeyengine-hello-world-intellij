74962c0e5e82eb9d6078da822b16abe55ed5fa6c
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2020-12-06 23:21:27 +0100

    - add new subpackage appstates - create new skeleton MainWithAppStates to show how to inject custom appstates

---
+++ src/main/java/com/asodc/example/appstates/MainWithAppStates.java
@@ -0,0 +1,20 @@
+package com.asodc.example.appstates;
+
+import com.jme3.app.SimpleApplication;
+
+public class MainWithAppStates extends SimpleApplication {
+
+    public MainWithAppStates() {
+        // TODO: pass in custom app states when they have been created
+        super();
+    }
+
+    public static void main(String... args) {
+        new MainWithAppStates().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // TODO: implement something as an example
+    }
+}
