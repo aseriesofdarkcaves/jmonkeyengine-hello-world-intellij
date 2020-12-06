b9b395906fec2f8258f2e3ef92f0a27d9d195eec
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2020-12-06 23:33:06 +0100

    - update pom to enforce build source & target java versions - add HelloNode skeleton class

---
+++ src/main/java/com/asodc/example/node/HelloNode.java
@@ -0,0 +1,14 @@
+package com.asodc.example.node;
+
+import com.jme3.app.SimpleApplication;
+
+public class HelloNode extends SimpleApplication {
+    public static void main(String... args) {
+        new HelloNode().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // TODO: implementation
+    }
+}
