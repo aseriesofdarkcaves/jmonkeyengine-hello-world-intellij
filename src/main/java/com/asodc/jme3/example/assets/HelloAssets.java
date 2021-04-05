d9555adf86d6663cc482aed86581966f6f62cb94
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-04-05 16:58:48 +0200

    add skeleton setup for HelloAssets tutorial

---
+++ src/main/java/com/asodc/jme3/example/assets/HelloAssets.java
@@ -0,0 +1,22 @@
+package com.asodc.jme3.example.assets;
+
+import com.jme3.app.SimpleApplication;
+
+public class HelloAssets extends SimpleApplication {
+    public static void main(String... args) {
+        new HelloAssets().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // TODO: add teapot with material
+
+        // TODO: add wall with texture
+
+        // TODO: display a line of text with default font
+
+        // TODO: load a model from test data
+
+        // TODO: add light
+    }
+}
---
+++ src/main/java/com/asodc/jme3/example/assets/README.md
@@ -0,0 +1,5 @@
+# [HelloAssets](https://wiki.jmonkeyengine.org/docs/3.3/tutorials/beginner/hello_asset.html)
+Learning how to load 3D models and text into the scene graph using the 
+JME AssetManager.
+
+TODO: because we're not using the SDK, some stuff may differ from the tutorial here
