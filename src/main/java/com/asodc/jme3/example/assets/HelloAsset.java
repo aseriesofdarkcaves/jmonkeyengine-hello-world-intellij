2beba90ef20fb208b6a5d89b2fed2da35b79a025
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-04-07 17:27:10 +0200

    cleanup HelloAsset

--- src/main/java/com/asodc/jme3/example/assets/HelloAsset.java
+++ src/main/java/com/asodc/jme3/example/assets/HelloAsset.java
@@ -16,31 +16,30 @@ public class HelloAsset extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        // TODO: add teapot with material
+        initTeapot();
+        initWall();
+        initFont();
+        initNinja();
+        initLighting();
+    }
 
-        // TODO: add wall with texture
+    private void initLighting() {
+        // You must add a light to make the model visible
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
+        rootNode.addLight(sun);
+    }
 
-        // TODO: display a line of text with default font
+    private void initNinja() {
+        // Load a model from test_data (OgreXML + material + texture)
+        Spatial ninja = assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
+        ninja.scale(0.05f, 0.05f, 0.05f);
+        ninja.rotate(0.0f, -3.0f, 0.0f);
+        ninja.setLocalTranslation(0.0f, -5.0f, -2.0f);
+        rootNode.attachChild(ninja);
+    }
 
-        // TODO: load a model from test data
-
-        // TODO: add light
-
-        // TODO: remove this copy-pasted stuff when I understand it/have refactored the code to my own words
-        Spatial teapot = assetManager.loadModel("Models/Teapot/Teapot.obj");
-        Material mat_default = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
-        teapot.setMaterial(mat_default);
-        rootNode.attachChild(teapot);
-
-        // Create a wall with a simple texture from test_data
-        Box box = new Box(2.5f, 2.5f, 1.0f);
-        Spatial wall = new Geometry("Box", box);
-        Material mat_brick = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat_brick.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
-        wall.setMaterial(mat_brick);
-        wall.setLocalTranslation(2.0f, -2.5f, 0.0f);
-        rootNode.attachChild(wall);
-
+    private void initFont() {
         // Display a line of text with a default font
         guiNode.detachAllChildren();
         guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
@@ -49,17 +48,23 @@ public class HelloAsset extends SimpleApplication {
         helloText.setText("Hello World");
         helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
         guiNode.attachChild(helloText);
+    }
 
-        // Load a model from test_data (OgreXML + material + texture)
-        Spatial ninja = assetManager.loadModel("Models/Ninja/Ninja.mesh.xml");
-        ninja.scale(0.05f, 0.05f, 0.05f);
-        ninja.rotate(0.0f, -3.0f, 0.0f);
-        ninja.setLocalTranslation(0.0f, -5.0f, -2.0f);
-        rootNode.attachChild(ninja);
+    private void initWall() {
+        // Create a wall with a simple texture from test_data
+        Box box = new Box(2.5f, 2.5f, 1.0f);
+        Spatial wall = new Geometry("Box", box);
+        Material mat_brick = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat_brick.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
+        wall.setMaterial(mat_brick);
+        wall.setLocalTranslation(2.0f, -2.5f, 0.0f);
+        rootNode.attachChild(wall);
+    }
 
-        // You must add a light to make the model visible
-        DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
-        rootNode.addLight(sun);
+    private void initTeapot() {
+        Spatial teapot = assetManager.loadModel("Models/Teapot/Teapot.obj");
+        Material mat_default = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
+        teapot.setMaterial(mat_default);
+        rootNode.attachChild(teapot);
     }
 }
