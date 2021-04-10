c5d8db832e7284b330857c3756e5987ed03ffb24
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-04-10 21:15:44 +0200

    cleanup and hide fields using method params

--- src/main/java/com/asodc/jme3/example/update/RollingCube.java
+++ src/main/java/com/asodc/jme3/example/update/RollingCube.java
@@ -11,8 +11,8 @@ public class RollingCube extends SimpleApplication {
 
     private float rotationTicks = 0f;
     private float translationTicks = 0f;
-    private float rotationRate = 5f;
-    private float translationRate = 2.5f;
+    private float rotationRate = 2f;
+    private float translationRate = 2f;
 
     public static void main(String... args) {
         new RollingCube().start();
@@ -22,11 +22,10 @@ public class RollingCube extends SimpleApplication {
     public void simpleInitApp() {
         Box cubeMesh = new Box(1f, 1f, 1f);
 
-        cubeGeometry = new Geometry("cube", cubeMesh);
-
         Material cubeMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         cubeMaterial.setColor("Color", ColorRGBA.Red);
 
+        cubeGeometry = new Geometry("cube", cubeMesh);
         cubeGeometry.setMaterial(cubeMaterial);
 
         rootNode.attachChild(cubeGeometry);
@@ -34,20 +33,20 @@ public class RollingCube extends SimpleApplication {
 
     @Override
     public void simpleUpdate(float tpf) {
-        doLockedMovement(tpf);
-//        doIndependentMovement(tpf);
+        doIndependentMovement(tpf, rotationRate, translationRate);
     }
 
     /**
      * Moves the cube in a manner where the rotation and translation rates are locked to the same value.
      *
      * @param tpf time per frame
+     * @param movementRate the rate of movement for rotation and translation
      */
-    private void doLockedMovement(float tpf) {
+    private void doLockedMovement(float tpf, float movementRate) {
         rotationTicks += tpf;
-        if (rotationTicks < rotationRate) {
-            cubeGeometry.rotate(rotationRate * tpf, 0f, 0f);
-            cubeGeometry.move(0f, 0f, rotationRate * tpf);
+        if (rotationTicks < movementRate) {
+            cubeGeometry.rotate(movementRate * tpf, 0f, 0f);
+            cubeGeometry.move(0f, 0f, movementRate * tpf);
             rotationTicks = 0;
         }
     }
@@ -57,16 +56,14 @@ public class RollingCube extends SimpleApplication {
      *
      * @param tpf time per frame
      */
-    private void doIndependentMovement(float tpf) {
+    private void doIndependentMovement(float tpf, float rotationRate, float translationRate) {
         rotationTicks += tpf;
 
-        // rotation part
         if (rotationTicks < rotationRate) {
             cubeGeometry.rotate(rotationRate * tpf, 0f, 0f);
             rotationTicks = 0f;
         }
 
-        // translation part
         if (translationTicks < translationRate) {
             cubeGeometry.move(0f, 0f, translationRate * tpf);
             translationTicks = 0f;
