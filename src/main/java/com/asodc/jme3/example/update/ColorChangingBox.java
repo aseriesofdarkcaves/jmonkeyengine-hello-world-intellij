01144cc9b63f2d74000c69cc3e04e086b28ce500
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-04-10 14:40:49 +0200

    add ColorChangingBox class

---
+++ src/main/java/com/asodc/jme3/example/update/ColorChangingBox.java
@@ -0,0 +1,19 @@
+package com.asodc.jme3.example.update;
+
+import com.jme3.app.SimpleApplication;
+
+public class ColorChangingBox extends SimpleApplication {
+    public static void main(String... args) {
+        new ColorChangingBox().start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        // TODO: init the application
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        // TODO: update the material
+    }
+}
